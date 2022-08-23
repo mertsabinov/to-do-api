728a3dc96acf243072da098cce9099674333d7f1
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-23 21:01:50 +0200

    add(user_services): add and search

--- internal/user_services/user_services.go
+++ internal/user_services/user_services.go
@@ -1,3 +1,23 @@
 package user_services
 
 type Db map[string]string
+
+func (db Db) Search(key string) (string, error) {
+	result, ok := db[key]
+	if !ok {
+		return "", UsKeyNotFound
+	}
+	return result, nil
+}
+
+func (db Db) Add(key string, value string) error {
+	_, err := db.Search(key)
+	switch err {
+	case nil:
+		return UsKeyAllReadyExist
+	case UsKeyNotFound:
+		db[key] = value
+		break
+	}
+	return nil
+}
