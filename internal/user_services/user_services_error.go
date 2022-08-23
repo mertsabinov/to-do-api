9a0e99fb134f2d125f1d286f572d2235babe731b
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-23 21:01:10 +0200

    add(user_services_error) Us key not found, us key all readt exist

---
+++ internal/user_services/user_services_error.go
@@ -0,0 +1,13 @@
+package user_services
+
+type UserServicesError string
+
+const (
+	UsKeyNotFound      UserServicesError = "This key is not found"
+	UsKeyAllReadyExist UserServicesError = "this key is all ready exist"
+)
+
+func (us UserServicesError) Error() string {
+	return string(us)
+}
+
