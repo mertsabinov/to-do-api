e26f16437eaa970b6b133dd35b31b0e794984bd3
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-23 21:01:36 +0200

    add(user_services_test): search and add

---
+++ internal/user_services/user_services_test.go
@@ -0,0 +1,37 @@
+package user_services
+
+import "testing"
+
+var testData = Db{
+	"test": "test",
+}
+
+func CheckError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func Check(t *testing.T, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got = %s want = %s", got, want)
+	}
+}
+
+func TestDb_Search(t *testing.T) {
+	got, err := testData.Search("test")
+	CheckError(t, err)
+	want := "test"
+	Check(t, got, want)
+}
+
+func TestDb_Add(t *testing.T) {
+	err := testData.Add("key", "value")
+	CheckError(t, err)
+	got, err := testData.Search("key")
+	CheckError(t, err)
+	want := "value"
+	Check(t, got, want)
+}
