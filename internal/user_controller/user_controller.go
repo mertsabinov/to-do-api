d98893c9e6d65471cb0d10bae2af6a78e32b62a1
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-24 08:56:43 +0200

    add(user_controller): add

--- internal/user_controller/user_controller.go
+++ internal/user_controller/user_controller.go
@@ -1,8 +1,10 @@
 package user_controller
 
 import (
+	"api/internal/model"
 	"api/internal/user_services"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -20,7 +22,20 @@ func (us *UserController) Ping(ctx *gin.Context) {
 	ctx.JSONP(http.StatusOK, gin.H{"message": "pong"})
 }
 
+func (us *UserController) Add(ctx *gin.Context) {
+	var todo model.Todo
+	ctx.ShouldBindJSON(&todo)
+	newId := uuid.NewString()
+	us.UserServices.Add(newId, todo)
+	result, err := us.UserServices.Search(newId)
+	if err != nil {
+		ctx.JSONP(http.StatusBadRequest, gin.H{"message": err})
+	}
+	ctx.JSONP(http.StatusOK, gin.H{"key": result.Key, "value": result.Value})
+}
+
 func (us *UserController) UserControllerRout(rg *gin.RouterGroup) {
 	userRoute := rg.Group("/user")
 	userRoute.GET("/ping", us.Ping)
+	userRoute.POST("/add", us.Add)
 }
