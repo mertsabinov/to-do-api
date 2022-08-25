9387c107bb87def2d02566a381b265e811f37ad9
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-25 19:36:48 +0200

    add(user_controller): delete

--- internal/user_controller/user_controller.go
+++ internal/user_controller/user_controller.go
@@ -2,6 +2,7 @@ package user_controller
 
 import (
 	"api/internal/model"
+	"api/internal/model/request_model"
 	"api/internal/user_services"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -24,14 +25,14 @@ func (us *UserController) Ping(ctx *gin.Context) {
 
 func (us *UserController) Add(ctx *gin.Context) {
 	var todo model.Todo
-	ctx.ShouldBindJSON(&todo)
+	err := ctx.ShouldBindJSON(&todo)
 	newId := uuid.NewString()
-	us.UserServices.Add(newId, todo)
+	err = us.UserServices.Add(newId, todo)
 	result, err := us.UserServices.Search(newId)
-	if err != nil {
-		ctx.JSONP(http.StatusBadRequest, gin.H{"message": err})
+	us.CheckError(ctx, err)
+	if err == nil {
+		ctx.JSONP(http.StatusOK, gin.H{"key": result.Key, "value": result.Value})
 	}
-	ctx.JSONP(http.StatusOK, gin.H{"key": result.Key, "value": result.Value})
 }
 
 func (us *UserController) GetAll(ctx *gin.Context) {
@@ -39,9 +40,27 @@ func (us *UserController) GetAll(ctx *gin.Context) {
 	ctx.JSONP(http.StatusOK, data)
 }
 
+func (us *UserController) Delete(ctx *gin.Context) {
+	var body request_model.RequestId
+	err := ctx.ShouldBindJSON(&body)
+	_, err = us.UserServices.Search(body.Id)
+	err = us.UserServices.Remove(body.Id)
+	us.CheckError(ctx, err)
+	if err == nil {
+		ctx.JSONP(http.StatusOK, gin.H{"message": "ok"})
+	}
+}
+
+func (us *UserController) CheckError(ctx *gin.Context, err error) {
+	if err != nil {
+		ctx.JSONP(http.StatusBadRequest, gin.H{"message": err})
+	}
+}
+
 func (us *UserController) UserControllerRout(rg *gin.RouterGroup) {
 	userRoute := rg.Group("/user")
 	userRoute.GET("/ping", us.Ping)
 	userRoute.POST("/add", us.Add)
 	userRoute.GET("/todo", us.GetAll)
+	userRoute.DELETE("/todo", us.Delete)
 }
