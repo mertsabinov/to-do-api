d646f89acfc0be209c30f450a00bdb5c00741d1c
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-26 10:04:14 +0200

    update(user_services): db type

--- internal/user_services/user_services.go
+++ internal/user_services/user_services.go
@@ -2,35 +2,54 @@ package user_services
 
 import (
 	"api/internal/model"
+	"api/internal/model/request_model"
 )
 
-type Db map[string]model.Todo
+type Db []model.Todo
 
-func (db Db) Search(key string) (model.Todo, error) {
-	result, ok := db[key]
-	if !ok {
-		return model.Todo{}, UsKeyNotFound
+func (db *Db) Search(id string) (model.Todo, error) {
+	var result model.Todo
+	for _, todo := range *db {
+		if todo.Id == id {
+			result = todo
+			break
+		}
+	}
+	err := db.CheckNill(result.Id)
+	if err != nil {
+		return result, err
 	}
 	return result, nil
 }
 
-func (db Db) Add(key string, value model.Todo) error {
-	_, err := db.Search(key)
+func (db *Db) Add(id string, todo request_model.RequestTodo) error {
+	newTodo := model.Todo{
+		Id:    id,
+		Key:   todo.Key,
+		Value: todo.Value,
+	}
+	_, err := db.Search(id)
 	switch err {
 	case nil:
 		return UsKeyAllReadyExist
 	case UsKeyNotFound:
-		db[key] = value
+		*db = append(*db, newTodo)
 		break
 	}
 	return nil
 }
 
-func (db Db) Remove(key string) error {
-	_, err := db.Search(key)
+func (db *Db) Remove(id string) error {
+	_, err := db.Search(id)
+	newDb := []model.Todo{}
 	switch err {
 	case nil:
-		delete(db, key)
+		for _, todo := range *db {
+			if todo.Id != id {
+				newDb = append(newDb, todo)
+			}
+		}
+		*db = newDb
 		break
 	case UsKeyNotFound:
 		return UsKeyNotFound
@@ -38,6 +57,13 @@ func (db Db) Remove(key string) error {
 	return nil
 }
 
-func (db Db) GetAll() Db {
-	return db
+func (db *Db) GetAll() Db {
+	return *db
+}
+
+func (db Db) CheckNill(id string) error {
+	if id == "" {
+		return UsKeyNotFound
+	}
+	return nil
 }
