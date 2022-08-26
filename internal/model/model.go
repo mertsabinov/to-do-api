cd488cad2a3741665fd77fbdf64eebe79431fd5b
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-26 10:03:41 +0200

    add(request_model): request todo

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,7 @@
 package model
 
 type Todo struct {
+	Id    string `json:"id"`
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
--- internal/model/request_model/request_model.go
+++ internal/model/request_model/request_model.go
@@ -3,3 +3,8 @@ package request_model
 type RequestId struct {
 	Id string
 }
+
+type RequestTodo struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
