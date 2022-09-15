b1352da575816761703e57379405bb9b3c0e1b17
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-15 16:00:21 +0300

    add(cors)

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,17 @@ import (
 
 func Start() *gin.Engine {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+	})
 	us := user_service.NewUserService([]model.Todo{})
 	uc := user_controller.NewUserConroller(us)
 	basepath := r.Group("/v1")
